ff36082abd0346064018861ac31211c6549ab160
Mohammed Gaber <gaberm@gmail.com>
2022-09-13 12:41:18 +0200

    Remove DEX password dependency

--- cdk/lib/config/env.ts
+++ cdk/lib/config/env.ts
@@ -3,4 +3,8 @@ import process = require("process");
 export class Env {
   static readonly account = process.env.CDK_DEFAULT_ACCOUNT;
   static readonly region = process.env.CDK_DEFAULT_REGION;
+
+  static readonly SAMPLE_USER_PASSWORD_ARN =
+    process.env.SAMPLE_USER_PASSWORD_ARN ??
+    "arn:aws:secretsmanager:us-west-2:xxx:secret:dexUserPassword-j5qJDL";
 }
